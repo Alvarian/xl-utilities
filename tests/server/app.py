aba0f85aaa9f0121ebb5b058bd7a008e9c0eb1b2
Alvarian <alvarivan88@gmail.com>
2022-12-10 08:01:51 -0500

    create vanilla version one / demo of client interface

---
+++ tests/server/app.py
@@ -0,0 +1,65 @@
+from xl_utility import formatter
+from xl_utility.formatter import *
+
+from inspect import getmembers, isfunction
+from io import BytesIO
+from flask import Flask, request, render_template, send_file
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = "development key"
+
+
+@app.route("/")
+def index():
+    # try:
+    #     return send_file(
+    #         "../demographics/main.xlsx", 
+    #         download_name="python.jpg",
+    #         mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    #     )
+    # except Exception as e:
+    #     return str(e)
+    omitted_functions = ["NamedTemporaryFile", "load_workbook", "get_column_letter"]
+    feature_names = list(filter(lambda x: bool(x[0].split("_")[0]) and not x[0].split()[0] in omitted_functions, getmembers(formatter, isfunction)))
+    return render_template("index.html", features=feature_names)
+
+@app.route("/formatter/capitalize_all", methods = ["POST"])
+def get_capitalized_column():
+    EXCEL_FILE = BytesIO(request.files["file"].read())
+
+    return send_file(
+        BytesIO(capitalize_all(request.form["columns_selected"].split(","), EXCEL_FILE)["buffer"]), 
+        download_name="new_sheet.xlsx",
+        mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+
+@app.route("/formatter/capitalize_firstletter", methods = ["POST"])
+def get_capitalized_first():
+    EXCEL_FILE = BytesIO(request.files["file"].read())
+
+    return send_file(
+        BytesIO(capitalize_firstLetter(request.form["columns_selected"].split(","), EXCEL_FILE)["buffer"]), 
+        download_name="new_sheet.xlsx",
+        mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+
+@app.route("/formatter/separate_addresses", methods = ["POST"])
+def get_separated_addresses():
+    EXCEL_FILE = BytesIO(request.files["file"].read())
+
+    return send_file(
+        BytesIO(separate_addresses(request.form["columns_selected"].split(","), EXCEL_FILE)["buffer"]), 
+        download_name="new_sheet.xlsx",
+        mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
+
+@app.route("/formatter/separate_names", methods = ["POST"])
+def get_separated_names():
+    EXCEL_FILE = BytesIO(request.files["file"].read())
+
+    return send_file(
+        BytesIO(separate_names(request.form["columns_selected"].split(","), EXCEL_FILE)["buffer"]), 
+        download_name="new_sheet.xlsx",
+        mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    )
